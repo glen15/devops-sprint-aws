bdf6d58fdf5fecf23646bde7f6309fd0c96bf7df
Kelly <68624696+yrkimyy@users.noreply.github.com>
2021-12-16 00:34:49 +0900

    fix: http-test test3

--- client/test/http-test.js
+++ client/test/http-test.js
@@ -14,16 +14,10 @@ describe('http - EC2와 S3 설정을 확인합니다.', () => {
     const title = result.data.includes('Deployment Practice');
     expect(title).to.be.eql(true);
   });
-
-  it('3. EC2와 S3 사이의 환경설정을 올바르게 설정해야 합니다.', async () => {
-    const result = await axios.get('https://yeoraebucket.s3.ap-northeast-2.amazonaws.com/static/js/2.2e1a1952.chunk.js');
-    const ec2url = result.data.includes(`${process.env.REACT_APP_API_URL}`);
-    expect(ec2url).to.be.eql(true);
-  });
 });
 
 describe('http - RDS, EC2 연결', () => {
-  it('4. RDS를 배포하고, EC2와 연결했으면, "/status" 요청에 대한 응답이 { isLogin: true, isConnectedToDatabase: true } 이어야 한다', async () => {
+  it('3. RDS를 배포하고, EC2와 연결했으면, "/status" 요청에 대한 응답이 { isLogin: true, isConnectedToDatabase: true } 이어야 한다', async () => {
     const res = await axios
       .post(
         `${process.env.REACT_APP_API_URL}/signin`,
